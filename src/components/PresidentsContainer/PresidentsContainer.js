05248c17615098eeeeb740cc8f4449aaffdad83e
Jeo D <e8gea6w5z8id@opayq.com>
2019-02-26 10:50:16 -0700

    Add party filter feature

--- src/components/PresidentsContainer/PresidentsContainer.js
+++ src/components/PresidentsContainer/PresidentsContainer.js
@@ -3,15 +3,45 @@ import './PresidentsContainer.css';
 import { PresidentCard } from '../PresidentCard/PresidentCard';
 
 export class PresidentsContainer extends Component {
-  render() {
+  constructor() {
+    super();
+    this.state = { 
+      partyFilter: 'All'
+    }
+  }
+
+  handleChange = (event) => {
+    this.setState({ partyFilter: event.target.value });
+  }
+
+  getPresidentsToDisplay = () => {
     const { presidents } = this.props;
+    const { partyFilter } = this.state;
+    if (partyFilter === 'All') {
+      return presidents.map(president => {
+        return <PresidentCard key={president.number} {...president} />
+      });
+    }
+    return presidents.filter(president => president.party === partyFilter)
+      .map(president => {
+        return <PresidentCard key={president.number} {...president} />
+      });
+  }
+
+  render() {
+    const { partyFilter } = this.state;
     return(
       <section>
-        {
-          presidents.map(president => {
-            return <PresidentCard key={president.number} {...president} />
-          })
-        }
+        <select onChange={this.handleChange} value={partyFilter}>
+          <option value="All">All</option>
+          <option value="No Party">No Party</option>
+          <option value="Federalist">Federalist</option>
+          <option value="Democratic-Republican">Democratic-Republican</option>
+          <option value="Whig">Whig</option>
+          <option value="Democratic">Democratic</option>
+          <option value="Republican">Republican</option>
+        </select>
+        {this.getPresidentsToDisplay()}
       </section>
     );
   }
