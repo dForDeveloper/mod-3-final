c82576eef0b8abc9e0a2cff32bcfc55a863b203d
Jeo D <e8gea6w5z8id@opayq.com>
2019-02-26 09:38:36 -0700

    Add hasErrored action creator and reducer

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,3 +2,8 @@ export const setIsLoading = (bool) => ({
   type: 'SET_IS_LOADING',
   isLoading: bool
 });
+
+export const setHasErrored = (message) => ({
+  type: 'SET_HAS_ERRORED',
+  message
+});
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux';
 import { isLoadingReducer } from './isLoadingReducer';
+import { hasErroredReducer } from './hasErroredReducer';
 
 export const rootReducer = combineReducers({
-  isLoading: isLoadingReducer
+  isLoading: isLoadingReducer,
+  hasErrored: hasErroredReducer
 });
