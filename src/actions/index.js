e1f8657c458aba92e78338161612602aadcf36d7
Jeo D <e8gea6w5z8id@opayq.com>
2019-02-26 09:43:41 -0700

    Add presidents action creator and reducer

--- src/actions/index.js
+++ src/actions/index.js
@@ -7,3 +7,8 @@ export const setHasErrored = (message) => ({
   type: 'SET_HAS_ERRORED',
   message
 });
+
+export const setPresidents = (presidents) => ({
+  type: 'SET_PRESIDENTS',
+  presidents
+});
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import { isLoadingReducer } from './isLoadingReducer';
 import { hasErroredReducer } from './hasErroredReducer';
+import { presidentsReducer } from './presidentsReducer';
 
 export const rootReducer = combineReducers({
   isLoading: isLoadingReducer,
-  hasErrored: hasErroredReducer
+  hasErrored: hasErroredReducer,
+  presidents: presidentsReducer
 });
