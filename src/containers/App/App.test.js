7810c585e77234113b1034a41fddb1bf5f33dc56
Jeo D <e8gea6w5z8id@opayq.com>
2019-02-26 10:24:47 -0700

    Test mapStateToProps and mapDispatchToProps

--- src/containers/App/App.test.js
+++ src/containers/App/App.test.js
@@ -1,9 +1,47 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import { mapStateToProps, mapDispatchToProps } from './App';
+import { setIsLoading, setHasErrored, setPresidents } from '../../actions';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe('mapStateToProps', () => {
+  it('should return a props object with the correct properties', () => {
+    const mockState = {
+      isLoading: false,
+      hasErrored: '',
+      presidents: [],
+      extraProperty: true
+    }
+    const expected = {
+      isLoading: false,
+      hasErrored: '',
+      presidents: [],
+    }
+    const result = mapStateToProps(mockState);
+    expect(result).toEqual(expected);
+  });
+});
+
+describe('mapDispatchToProps', () => {
+  const mockDispatch = jest.fn();
+  const props = mapDispatchToProps(mockDispatch);
+
+  it('should call dispatch with setIsLoading', () => {
+    const expected = setIsLoading(true);
+    props.setIsLoading(true);
+    expect(mockDispatch).toHaveBeenCalledWith(expected);
+  });
+
+  it('should call dispatch with setHasErrored', () => {
+    const expected = setHasErrored('Not found');
+    props.setHasErrored('Not found');
+    expect(mockDispatch).toHaveBeenCalledWith(expected);
+  });
+
+  it('should call dispatch with setPresidents', () => {
+    const mockPresidents = [
+      { president: 'George W. Bush'},
+      { president: 'Donald J. Trump'}
+    ];
+    const expected = setPresidents(mockPresidents);
+    props.setPresidents(mockPresidents);
+    expect(mockDispatch).toHaveBeenCalledWith(expected);
+  });
 });
