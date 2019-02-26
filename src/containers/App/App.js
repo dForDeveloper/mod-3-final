1764ce14e5445913d0b226074913011f05f0553d
Jeo D <e8gea6w5z8id@opayq.com>
2019-02-26 09:44:12 -0700

    Add method for fetching presidents data

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,35 +1,46 @@
 import React, { Component } from 'react';
 import './App.css';
 import { connect } from 'react-redux';
+import { setIsLoading, setHasErrored, setPresidents } from '../../actions';
 
 export class App extends Component {
+  componentDidMount() {
+    this.fetchPresidents();
+  }
+
+  fetchPresidents = async () => {
+    const { setIsLoading, setHasErrored, setPresidents } = this.props;
+    setIsLoading(true);
+    const url = 'http://localhost:3001/api/v1/presidents';
+    const response = await fetch(url);
+    if (!response.ok) {
+      setHasErrored(response.statusText);
+    } else {
+      const presidents = await response.json();
+      setPresidents(presidents);
+    }
+    setIsLoading(false);
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>Presidents and Assholes</h1>
       </div>
     );
   }
 }
 
 export const mapStateToProps = (state) => ({
-  isLoading: state.isLoading
+  isLoading: state.isLoading,
+  hasErrored: state.hasErrored,
+  presidents: state.presidents
 });
 
 export const mapDispatchToProps = (dispatch) => ({
-
+  setIsLoading: (bool) => dispatch(setIsLoading(bool)),
+  setHasErrored: (message) => dispatch(setHasErrored(message)),
+  setPresidents: (presidents) => dispatch(setPresidents(presidents))
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(App);
