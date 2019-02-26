692392d854f717a8a43f847f27828ca6d3b7dcc6
Jeo D <e8gea6w5z8id@opayq.com>
2019-02-26 09:53:50 -0700

    Add PresidentsContainer component

---
+++ src/components/PresidentsContainer/PresidentsContainer.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './PresidentsContainer.css';
+
+export class PresidentsContainer extends Component {
+  render() {
+    return(
+      <section>
+        
+      </section>
+    );
+  }
+}
