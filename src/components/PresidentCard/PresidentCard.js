19d815d2144706df076bf6cb95c3b6440ab77b95
Jeo D <e8gea6w5z8id@opayq.com>
2019-02-26 10:00:54 -0700

    Add PresidentCard component

---
+++ src/components/PresidentCard/PresidentCard.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './PresidentCard.css';
+
+export const PresidentCard = (props) => {
+  const { birth_year, death_year, left_office, number, party, president, took_office } = props;
+  return(
+    <article>
+      <h3>#{number}: {president}</h3>
+      <ul>
+        <li>Party: {party}</li>
+        <li>Born: {birth_year}</li>
+        <li>Died: {death_year}</li>
+        <li>Took Office: {took_office}</li>
+        <li>Left Office: {left_office}</li>
+      </ul>
+    </article>
+  )
+}
--- src/components/PresidentsContainer/PresidentsContainer.js
+++ src/components/PresidentsContainer/PresidentsContainer.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
 import './PresidentsContainer.css';
+import { PresidentCard } from '../PresidentCard/PresidentCard';
 
 export class PresidentsContainer extends Component {
   render() {
+    const { presidents } = this.props;
     return(
       <section>
-        
+        {
+          presidents.map(president => {
+            return <PresidentCard key={president.number} {...president} />
+          })
+        }
       </section>
     );
   }
